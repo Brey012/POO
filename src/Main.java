7b7e0e24fb344e2811413dfe7674a39e3d49e4bf
Breyner012 <bhernandezos@cesde.net>
2024-08-18 01:35:10 -0500

    Ya definí la base de la POO, que es una clase y lo que la compone.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
 public class Main {
+
+//    Voy a definir mi molde, o sea mi clase, definir sus atributos y sus métodos.
+static class Carro {
+//        Estos son los atributos de mi clase, de mi carro
+//    Los atributos vienen siendo características de mi carro(clase)
+        String color;
+        String modelo;
+        String marca;
+        int potencia;
+
+//        Estas son las acciones que puede hacer mi carro
+//    estas son los métodos(funciones), mensajes que llaman a mi clase para que cumplan una acción
+        void acelerar(){
+            System.out.println("El carro esta avanzando");
+        }
+
+        void frenar(){
+            System.out.println("El carro esta parando");
+        }
+}
+
     public static void main(String[] args) {
+//     aquí ya llamamos el método constructor Carro con la palabra clave new para indicar que estamos
+//     creando una nueva instancia en la clase Carro
+        Carro carro1 = new Carro();
+        carro1.color = "rojo";
+        carro1.marca = "Porsche";
+        carro1.modelo = "Gt3 rs";
+        carro1.potencia = 525;
 
-//    Bueno aquí explicaré por medio de código como es la POO
-
+        carro1.acelerar();
+        carro1.frenar();
     }
 }
